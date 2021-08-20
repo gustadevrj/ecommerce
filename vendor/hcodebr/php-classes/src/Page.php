932230a523ea93ce44e58c36ee87e546998f8aac
Gustavo Oliveira <jgugarj@gmail.com>
2021-08-20 03:50:49 -0300

    CRUD Usurios (Users) - Atualizacao dos Path (hosts e httpd-vhosts.conf)

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -20,8 +20,8 @@ class Page{
 
 		// config
 		$config = array(
-			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"] . "/UDEMY/curso_004/projeto_ecommerce" . $tpl_dir,
-			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"] . "/UDEMY/curso_004/projeto_ecommerce" . "/views-cache/",
+			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"] . $tpl_dir,
+			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"] . "/views-cache/",
 			"debug"         => true // set to false to improve the speed
 		);
 
