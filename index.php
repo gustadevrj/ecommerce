ca40b6538dd4e803970b30b55f0d3ca55bcc9b7f
Gustavo Oliveira <jgugarj@gmail.com>
2021-08-21 00:35:50 -0300

    Recuperar Senha

--- index.php
+++ index.php
@@ -196,10 +196,94 @@ $app->post('/admin/users/:iduser', function($iduser) {
 
 	exit;
 
-	//
-	$page = new PageAdmin();
+});
 
-	$page->setTpl("users-update");
+//Rota FORGOT PASSWORD - GET
+$app->get('/admin/forgot', function() {
+
+	//
+	$page = new PageAdmin([
+		"header"=>false, 
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot");
+
+});
+
+//Rota FORGOT PASSWORD - POST
+$app->post('/admin/forgot', function() {
+
+	//
+	$user = User::getForgot($_POST["email"]);
+
+	header("Location: /admin/forgot/sent");
+
+	exit;
+
+});
+
+//Rota FORGOT SENT
+$app->get('/admin/forgot/sent', function() {
+
+	//
+	$page = new PageAdmin([
+		"header"=>false, 
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-sent");
+
+});
+
+//Rota FORGOT RESET
+$app->get('/admin/forgot/reset', function() {
+
+	//
+	$user = User::validForgotDecrypt($_GET["code"]);
+
+	//
+	$page = new PageAdmin([
+		"header"=>false, 
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-reset", array(
+		"name"=>$user["desperson"], 
+		"code"=>$_GET["code"]
+	));
+
+});
+
+//Rota FORGOT RESET - POST
+$app->post('/admin/forgot/reset', function() {
+
+	//
+	$forgot = User::validForgotDecrypt($_POST["code"]);
+
+	//
+	User::setForgotUsed($forgot["idrecovery"]);
+
+	//
+	$user = new User();
+
+	$user->get((int)$forgot["iduser"]);
+
+	//
+	$password = password_hash($_POST["password"], PASSWORD_DEFAULT, [
+		"cost"=>12
+	]);
+
+	$user->setPassword($password);
+
+	//
+	$page = new PageAdmin([
+		"header"=>false, 
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-reset-success");
+
 });
 
 //
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -4,10 +4,19 @@ namespace Hcode\Model;
 
 use \Hcode\DB\Sql;
 use \Hcode\Model;
+use \Hcode\Mailer;
 
 class User extends Model{
 
 	const SESSION = "User";
+
+	const SECRET = "FEDCBA9876543210";
+
+	const SECRET_IV = "0123456789ABCDEF";
+
+	const ERROR = "UserError";
+	const ERROR_REGISTER = "UserErrorRegister";
+	const SUCCESS = "UserSucesss";
 
 	public static function login($login, $senha){
 
@@ -134,6 +143,332 @@ class User extends Model{
 			":iduser" => $this->getiduser()
 		));
 	}
+
+	public static function getForgot($email){
+
+		$sql = new Sql();
+
+		$result = $sql->select("
+				SELECT * FROM tb_persons a 
+				INNER JOIN tb_users b 
+				USING (idperson) 
+				WHERE a.desemail = :email;
+			", array(
+					":email"=>$email
+				));
+
+		if (count($result) === 0){
+			throw new \Exception("Não Foi Possivel Recuperar a Senha!");
+		}
+		else{
+
+			$dado = $result[0];
+
+			$result2 = $sql->select("CALL sp_userspasswordsrecoveries_create(:iduser, :desip)", array(
+					":iduser"=>$dado["iduser"], 
+					":desip"=>$_SERVER["REMOTE_ADDR"]
+			));
+
+			if(count($result2) === 0){
+				throw new \Exception("Não Foi Possivel Recuperar a Senha!");
+			}
+			else{
+				$dadoRecovery = $result2[0];
+
+				//
+				//Funcao mcrypt_encrypt() Foi Depreciada no PHP 7.1
+				//Se Ocorrer Algum Erro, Usar a Função openssl_encrypt()
+				//mcrypt_encrypt(cipher, key, data, mode)
+				//openssl_encrypt(data, method, password)
+				//openssl_encrypt(data, method, password, 0, ???)
+
+				//NAO FUNCIONA
+				//$code = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_128, User::SECRET, $dadoRecovery["idrecovery"], MCRYPT_MODE_ECB));
+
+				//FUNCIONA!
+				//$code = base64_encode(openssl_encrypt($dadoRecovery["idrecovery"], "AES-128-CBC", User::SECRET, 0, User::SECRET_IV));
+
+				//FUNCIONA!
+				$code = openssl_encrypt($dadoRecovery["idrecovery"], "AES-128-CBC", pack("a16", User::SECRET), 0, pack("a16", User::SECRET_IV));
+
+				$code = base64_encode($code);
+
+				$link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=" . $code;
+
+				/*
+				echo("<br>***<pre>");
+				print_r($link);
+				echo("</pre>***<br>");
+				//exit;
+				*/
+
+				//
+				$mailer = new Mailer($dado["desemail"], $dado["desperson"], "Redefinir Senha!", "forgot", array(
+						"name"=>$dado["desperson"], 
+						"link"=>$link
+				));
+
+				/*
+				echo("<br>***<pre>");
+				print_r($mailer);
+				echo("</pre>***<br>");
+				*/
+
+				/*
+				echo("<br>***<pre>");
+				print_r($dado);
+				echo("</pre>***<br>");
+				exit;
+				*/
+
+				//
+				$mailer->send();
+
+				return $dado;
+				//return $link;
+
+			}
+		}
+
+	}
+
+	public static function validForgotDecrypt($code){
+
+		$code = base64_decode($code);
+
+		//
+		//Funcao mcrypt_encrypt() Foi Depreciada no PHP 7.1
+		//Se Ocorrer Algum Erro, Usar a Função openssl_decrypt()
+		//mcrypt_decrypt(cipher, key, data, mode)
+		//openssl_decrypt(data, method, password)
+		//openssl_decrypt(data, method, password, 0, ???)
+
+		$idrecovery = openssl_decrypt($code, 'AES-128-CBC', pack("a16", User::SECRET), 0, pack("a16", User::SECRET_IV));
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+			SELECT *
+			FROM tb_userspasswordsrecoveries a
+			INNER JOIN tb_users b USING(iduser)
+			INNER JOIN tb_persons c USING(idperson)
+			WHERE
+				a.idrecovery = :idrecovery
+				AND
+				a.dtrecovery IS NULL
+				AND
+				DATE_ADD(a.dtregister, INTERVAL 1 HOUR) >= NOW();", 
+			array(
+				":idrecovery"=>$idrecovery
+			));
+
+		if (count($results) === 0){
+			throw new \Exception("Não foi possível recuperar a senha.");
+		}
+		else{
+
+			return $results[0];
+
+		}
+
+	}
+
+	public static function setForgotUsed($idrecovery){
+
+		$sql = new Sql();
+
+		$sql->query("UPDATE tb_userspasswordsrecoveries SET dtrecovery = NOW() WHERE idrecovery = :idrecovery", array(
+			":idrecovery"=>$idrecovery
+		));
+
+	}
+
+	public function setPassword($password){
+
+		$sql = new Sql();
+
+		$sql->query("UPDATE tb_users SET despassword = :password WHERE iduser = :iduser", array(
+			":password"=>$password,
+			":iduser"=>$this->getiduser()
+		));
+
+	}
+
+	//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+	public static function setError($msg)
+	{
+
+		$_SESSION[User::ERROR] = $msg;
+
+	}
+
+	public static function getError()
+	{
+
+		$msg = (isset($_SESSION[User::ERROR]) && $_SESSION[User::ERROR]) ? $_SESSION[User::ERROR] : '';
+
+		User::clearError();
+
+		return $msg;
+
+	}
+
+	public static function clearError()
+	{
+
+		$_SESSION[User::ERROR] = NULL;
+
+	}
+
+	public static function setSuccess($msg)
+	{
+
+		$_SESSION[User::SUCCESS] = $msg;
+
+	}
+
+	public static function getSuccess()
+	{
+
+		$msg = (isset($_SESSION[User::SUCCESS]) && $_SESSION[User::SUCCESS]) ? $_SESSION[User::SUCCESS] : '';
+
+		User::clearSuccess();
+
+		return $msg;
+
+	}
+
+	public static function clearSuccess()
+	{
+
+		$_SESSION[User::SUCCESS] = NULL;
+
+	}
+
+	public static function setErrorRegister($msg)
+	{
+
+		$_SESSION[User::ERROR_REGISTER] = $msg;
+
+	}
+
+	public static function getErrorRegister()
+	{
+
+		$msg = (isset($_SESSION[User::ERROR_REGISTER]) && $_SESSION[User::ERROR_REGISTER]) ? $_SESSION[User::ERROR_REGISTER] : '';
+
+		User::clearErrorRegister();
+
+		return $msg;
+
+	}
+
+	public static function clearErrorRegister()
+	{
+
+		$_SESSION[User::ERROR_REGISTER] = NULL;
+
+	}
+
+	public static function checkLoginExist($login)
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :deslogin", [
+			':deslogin'=>$login
+		]);
+
+		return (count($results) > 0);
+
+	}
+
+	public static function getPasswordHash($password)
+	{
+
+		return password_hash($password, PASSWORD_DEFAULT, [
+			'cost'=>12
+		]);
+
+	}
+
+	public function getOrders()
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+			SELECT * 
+			FROM tb_orders a 
+			INNER JOIN tb_ordersstatus b USING(idstatus) 
+			INNER JOIN tb_carts c USING(idcart)
+			INNER JOIN tb_users d ON d.iduser = a.iduser
+			INNER JOIN tb_addresses e USING(idaddress)
+			INNER JOIN tb_persons f ON f.idperson = d.idperson
+			WHERE a.iduser = :iduser
+		", [
+			':iduser'=>$this->getiduser()
+		]);
+
+		return $results;
+
+	}
+
+	public static function getPage($page = 1, $itemsPerPage = 10)
+	{
+
+		$start = ($page - 1) * $itemsPerPage;
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+			SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_users a 
+			INNER JOIN tb_persons b USING(idperson) 
+			ORDER BY b.desperson
+			LIMIT $start, $itemsPerPage;
+		");
+
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+		return [
+			'data'=>$results,
+			'total'=>(int)$resultTotal[0]["nrtotal"],
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+		];
+
+	}
+
+	public static function getPageSearch($search, $page = 1, $itemsPerPage = 10)
+	{
+
+		$start = ($page - 1) * $itemsPerPage;
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+			SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_users a 
+			INNER JOIN tb_persons b USING(idperson)
+			WHERE b.desperson LIKE :search OR b.desemail = :search OR a.deslogin LIKE :search
+			ORDER BY b.desperson
+			LIMIT $start, $itemsPerPage;
+		", [
+			':search'=>'%'.$search.'%'
+		]);
+
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+		return [
+			'data'=>$results,
+			'total'=>(int)$resultTotal[0]["nrtotal"],
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+		];
+
+	}
+
+	//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 }
 
