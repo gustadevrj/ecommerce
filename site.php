42ad706b8d475755d0ca38555e068596c059cb42
Gustavo Oliveira <jgugarj@gmail.com>
2021-09-09 04:16:08 -0300

    SITE: Meus Pedidos

--- site.php
+++ site.php
@@ -345,7 +345,6 @@ $app->post('/checkout', function() {
 		"idaddress"=>$address->getidaddress(), 
 		"iduser"=>$user->getiduser(), 
 		"idstatus"=>OrderStatus::EM_ABERTO, 
-		//"vltotal"=>$totals["vlprice"] + $cart->getvlfreight() 
 		"vltotal"=>$totals 
 	]);
 
@@ -714,7 +713,7 @@ $app->get('/order/:idorder', function($idorder) {
 
 });
 
-//Rota ORDER - GET
+//Rota BOLETO - GET
 $app->get('/boleto/:idorder', function($idorder) {
 
 	//
@@ -732,10 +731,11 @@ $app->get('/boleto/:idorder', function($idorder) {
 	$dias_de_prazo_para_pagamento = 10;
 	$taxa_boleto = 5.00;
 	$data_venc = date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias OU informe data: "13/04/2006"; 
-	$valor_cobrado = $order->getvltotal(); // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
+	
+	$valor_cobrado = formatPrice($order->getvltotal()); // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
+	$valor_cobrado = str_replace(".", "",$valor_cobrado);
 	$valor_cobrado = str_replace(",", ".",$valor_cobrado);
 	$valor_boleto=number_format($valor_cobrado+$taxa_boleto, 2, ',', '');
-	//$valor_boleto = formatPrice($valor_cobrado + $taxa_boleto);
 
 	$dadosboleto["nosso_numero"] = $order->getidorder();  // Nosso numero - REGRA: Máximo de 8 caracteres!
 	$dadosboleto["numero_documento"] = $order->getidorder();	// Num do pedido ou nosso numero
@@ -798,6 +798,52 @@ $app->get('/boleto/:idorder', function($idorder) {
 	//******************************
 	//##############################
 	//##############################
+
+	exit;
+
+});
+
+//Rota PROFILE - ORDERS - GET
+$app->get('/profile/orders', function() {
+
+	//
+	User::verificaLogin(false);
+
+	$user = User::getfromSession();
+
+	$page = new Page();
+
+	$page->setTpl("profile-orders", array(
+		"orders"=>$user->getOrders() 
+	));
+
+	exit;
+
+});
+
+//Rota PROFILE - ORDERS - IDORDER - GET
+$app->get('/profile/orders/:idorder', function($idorder) {
+
+	//
+	User::verificaLogin(false);
+
+	$order = new Order();
+
+	$order->get((int)$idorder);
+
+	$cart = new Cart();
+
+	$cart->get((int)$order->getidcart());
+
+	$cart->getCalculateTotal();
+
+	$page = new Page();
+
+	$page->setTpl("profile-orders-detail", array(
+		"order"=>$order->getValues(), 
+		"cart"=>$cart->getValues(), 
+		"products"=>$cart->getProducts() 
+	));
 
 	exit;
 
