a5e83c3defd43a3fa69899dc3b628f245dcf9b05
Gustavo Oliveira <jgugarj@gmail.com>
2021-09-14 01:03:19 -0300

    ADMIN: Paginacao e Pesquisa (Busca) de Produtos

--- admin-products.php
+++ admin-products.php
@@ -10,13 +10,46 @@ $app->get("/admin/products", function(){
 	//
 	User::verificaLogin();
 
-	$products = Product::listAll();
+	//
+	$search = (isset($_GET["search"])) ? $_GET["search"] : "";
+	$page = (isset($_GET["page"])) ? (int)$_GET["page"] : 1;
+
+	//
+	if($search != ""){
+
+		//
+		$paginacao = Product::getPageSearch($search, $page);
+
+	}
+	else{
+
+		//
+		$paginacao = Product::getPage($page);
+
+	}
+
+	//
+	$pages = array();
+
+	for($x = 0; $x < $paginacao["pages"]; $x++){
+
+		array_push($pages, [
+			"href"=>"/admin/products?" . http_build_query([
+				"page"=>$x+1, 
+				"search"=>$search 
+			]), 
+			"text"=>$x+1 
+		]);
+
+	}
 
 	//
 	$page = new PageAdmin();
 
 	$page->setTpl("products", [
-		"products"=>$products
+		"products"=>$paginacao["dados"], 
+		"search"=>$search, 
+		"pages"=>$pages 
 	]);
 
 	exit;
--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -220,6 +220,59 @@ class Product extends Model{
 
 	}
 
+	//PAGINACAO
+	public static function getPage($page = 1, $itemsPerPage = 5){
+
+		//
+		$start = ($page - 1) * $itemsPerPage;
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+			SELECT SQL_CALC_FOUND_ROWS * FROM 
+			tb_products 
+			ORDER BY desproduct 
+			LIMIT $start, $itemsPerPage;
+			");
+
+		//QUANTOS ITENS TEM?
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() AS total;");
+
+		return array(
+			"dados"=>$results, 
+			"total"=>(int)$resultTotal[0]["total"], 
+			"pages"=>ceil($resultTotal[0]["total"] / $itemsPerPage)
+		);
+	}
+
+	//
+	public static function getPageSearch($search, $page = 1, $itemsPerPage = 5){
+
+		$start = ($page - 1) * $itemsPerPage;
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+			SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_products 
+			WHERE 
+			desproduct LIKE :search 
+			ORDER BY desproduct 
+			LIMIT $start, $itemsPerPage;
+		", [
+			':search'=>'%'.$search.'%'
+		]);
+
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+		return [
+			'dados'=>$results,
+			'total'=>(int)$resultTotal[0]["nrtotal"],
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+		];
+
+	}
+
 }
 
 ?>
